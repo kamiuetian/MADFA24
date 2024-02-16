53d3631f7152f6e4800413eb76ed5f312e838d9f
kamiuetian <kami_se01@live.com>
2024-02-16 14:09:10 +0500

    first commit of changes

---
+++ TheoryClass/Lec2(15-02)/myscript.js
@@ -0,0 +1,15 @@
+//console.log("hello world from separate file");
+/**Variables
+ *
+ * int abc=34  ;
+ * abc="string"
+ * int abc="abc"
+ *
+ */
+
+let abc = "abc";
+console.log(typeof abc);
+abc = 35;
+console.log(typeof abc);
+const xyz = "abc";
+xyz = 34;
